e8dd48e62aed78193b3af001683fc93dce3420f7
br1lisboa <brlisbo@gmail.com>
2022-06-24 19:02:53 -0300

    agregados algunos espacios por convencion

--- src/App.js
+++ src/App.js
@@ -12,21 +12,21 @@ function App() {
           imagen='mr'
           cargo='Ingeniero de Software'
           empresa='Spotify'
-          testimonio='Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusamus corporis asperiores distinctio alias praesentium aut? Suscipit magnam quidem debitis velit distinctio maxime recusandae illo iusto non reiciendis, similique atque.'/>
+          testimonio='Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusamus corporis asperiores distinctio alias praesentium aut? Suscipit magnam quidem debitis velit distinctio maxime recusandae illo iusto non reiciendis, similique atque.' />
         <Testimonio
           nombre='Joker'
           pais='EE UU'
           imagen='joker'
           cargo='RRPP'
           empresa='Facebook'
-          testimonio='Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusamus corporis asperiores distinctio alias praesentium aut? Suscipit magnam quidem debitis velit distinctio maxime recusandae illo iusto non reiciendis, similique atque.'/>
+          testimonio='Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusamus corporis asperiores distinctio alias praesentium aut? Suscipit magnam quidem debitis velit distinctio maxime recusandae illo iusto non reiciendis, similique atque.' />
         <Testimonio
           nombre='The Brad'
           pais='EE UU'
           imagen='brad'
           cargo='Gerente General'
           empresa='Walmart'
-          testimonio='Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusamus corporis asperiores distinctio alias praesentium aut? Suscipit magnam quidem debitis velit distinctio maxime recusandae illo iusto non reiciendis, similique atque.'/>
+          testimonio='Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusamus corporis asperiores distinctio alias praesentium aut? Suscipit magnam quidem debitis velit distinctio maxime recusandae illo iusto non reiciendis, similique atque.' />
       </div>
     </div>
   );
